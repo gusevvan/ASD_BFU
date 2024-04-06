0fcfb3c0636112cddcc52c590313b15514b5e68a
gusevvan <97231756+gusevvan@users.noreply.github.com>
2024-04-06 08:23:54 +0200

    solve lab18

--- fourthTerm/lab18/main.py
+++ fourthTerm/lab18/main.py
@@ -1,8 +1,18 @@
 n = int(input())
-x = list(map(int, input().split(' ')))
-s = [0]
-for i in range(n):
-    t = []
-    for j in range(len(s)):
-        t.append(s[j] + x[i])
-
+a = list(map(int, input().split(' ')))
+a.sort(key=lambda x: abs(x))
+cur_sum = a[0]
+min_sum = a[0]
+a = a[1:]
+for i in range(n - 1):
+    diffs = []
+    for j in range(len(a)):
+        diffs.append((abs(cur_sum + a[j]), j))
+    cur_sum = diffs[0][0]
+    min_sum = min(min_sum, cur_sum)
+    new_a = []
+    for j in range(len(a)):
+        if j != diffs[0][1]:
+            new_a.append(a[j])
+    a = new_a
+print(min_sum)
